1a2af8d9daba8f325e96a648957a734b85a7e4be
Sal Tamay <sal.tamay@shopify.com>
2023-07-28 12:12:18 -0400

    Add tailwind and svelte-flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<Alert>
+	<span class="font-medium">Default alert!</span> Change a few things up and try submitting again.
+</Alert>
